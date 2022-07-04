6b392ec9e1a802c3ae995094fe5468dff3a19a54
Benjamin George Obbo <obbobenjamin@gmail.com>
2022-07-04 23:08:14 +0000

    low level language c commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *  * main- Entry point
+ *   *
+ *    * return: Always 0(success)
+ *     */
+int main (void)
+{     
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	printf("size of a char: %lu byte(s)\n",(unsigned long) size of (d));
+	printf("size of an int: %lu byte(s)\n",(unsigned long) size of (a));
+	printf("size of a long int: %lu byte(s)\n",(unsigned long) size of (b));
+	printf("size of a long long int: %lu byte(s)\n",(unsigned long) size of (d));
+	printf("size of a float: %lu byte(s)\n",(unsigned long) size of (f));
+	return (0);
+}
