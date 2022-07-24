7b851b7a732bf868e12a36021549252c24e7c6d3
Benjamin George Obbo <obbobenjamin@gmail.com>
2022-07-24 20:39:17 +0000

    argc and arg v

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
 /**
- *  main:- writes a programm tht prints its name
- *  if you rename the progrm it prints the new name and compiles it again
- *  the path efor the program is not removed
- *  @argc: the argument count
- *  @argv: argument vector
- *  return: this return to 0
- */
+*  main:- writes a programm tht prints its name
+*  if you rename the progrm it prints the new name and compiles it again
+*  the path efor the program is not removed
+*  @argc: the argument count
+*  @argv: argument vector
+*  Return: this return to 0
+*/
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
